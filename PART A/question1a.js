1cb2b2c710c34f2e63a9ac7aa00559d198063d4a
Ethan <225321115+GeorgeChrisSmith@users.noreply.github.com>
2025-10-31 15:16:38 +1100

    Create question1a.js

---
+++ PART A/question1a.js
@@ -0,0 +1,5 @@
+/* ------------------------------
+   Step 1: Build an array
+   ------------------------------ */
+// Initial array of numbers
+let numberArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
